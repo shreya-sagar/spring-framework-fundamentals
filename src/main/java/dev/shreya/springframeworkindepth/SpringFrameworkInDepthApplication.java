902d57df7298bd32e054b785517ab5b9cb23dac9
Shreya Sagar <shreyasagar1705@gmail.com>
2023-08-22 23:09:45 +0530

    initial commit 2

---
+++ src/main/java/dev/shreya/springframeworkindepth/SpringFrameworkInDepthApplication.java
@@ -0,0 +1,13 @@
+package dev.shreya.springframeworkindepth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkInDepthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkInDepthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/shreya/springframeworkindepth/SpringFrameworkInDepthApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.shreya.springframeworkindepth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkInDepthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
